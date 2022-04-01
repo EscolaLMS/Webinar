e39ef35ab5be226cf533b4d31c8dea3f69934380
Hubert Krzysztofiak <hubert.krzysztofiak@escolasoft.com>
2022-04-01 15:20:44 +0200

    Add seeder to webinar terms

---
+++ database/seeders/WebinarsTermsSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace EscolaLms\Webinar\Database\Seeders;
+
+use EscolaLms\Core\Models\User;
+use EscolaLms\Webinar\Enum\WebinarStatusEnum;
+use EscolaLms\Webinar\Models\Webinar;
+use Illuminate\Database\Seeder;
+
+class WebinarsTermsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = User::limit(10)->get();
+        $trainers = User::limit(5)->get();
+
+        Webinar::factory(5, [
+            'status' => WebinarStatusEnum::PUBLISHED,
+            'active_to' => now()->modify('+1 hour'),
+            'duration' => '1 hours'
+        ])
+            ->create()
+            ->each(function (Webinar $webinar) use($users, $trainers) {
+                $webinar->users()->sync($users->pluck('id')->toArray());
+                $webinar->trainers()->sync($trainers->pluck('id')->toArray());
+            });
+    }
+}
