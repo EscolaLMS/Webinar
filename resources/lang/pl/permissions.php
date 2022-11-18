e5fadf34b0ec04393376cd146334fb2054b84e03
Maciej Rymarz <maciej.rymarz@escolasoft.com>
2022-11-18 16:26:58 +0100

    Add translations

---
+++ resources/lang/pl/permissions.php
@@ -0,0 +1,11 @@
+<?php
+
+use EscolaLms\Webinar\Enum\WebinarPermissionsEnum;
+
+return [
+    WebinarPermissionsEnum::WEBINAR_LIST => 'Lista webinarów',
+    WebinarPermissionsEnum::WEBINAR_CREATE => 'Utwórz webinar',
+    WebinarPermissionsEnum::WEBINAR_UPDATE => 'Aktualizuj webinar',
+    WebinarPermissionsEnum::WEBINAR_DELETE => 'Usuń webinar',
+    WebinarPermissionsEnum::WEBINAR_READ => 'Wyświetl webinar',
+];
