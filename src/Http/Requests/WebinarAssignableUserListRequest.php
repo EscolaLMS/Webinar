e608eb3b177031e5d25652b1b5fdc3f34aa2dea3
Witold Wiśniewski <witold.michal.wisniewski@gmail.com>
2023-03-29 16:22:41 +0200

    Added assignable users endpoint

---
+++ src/Http/Requests/WebinarAssignableUserListRequest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace EscolaLms\Webinar\Http\Requests;
+
+use EscolaLms\Webinar\Enum\WebinarPermissionsEnum;
+use EscolaLms\Webinar\Models\Webinar;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Gate;
+
+class WebinarAssignableUserListRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return Gate::allows(WebinarPermissionsEnum::WEBINAR_CREATE, Webinar::class);
+    }
+
+    public function rules(): array
+    {
+        return [];
+    }
+}
